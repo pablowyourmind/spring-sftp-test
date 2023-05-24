2ff80345e42e5fefb97064ee7b8365cb5e5dddc3
Pawel <46264807+pablowyourmind@users.noreply.github.com>
2023-05-24 23:35:00 +0200

    Initial commit

---
+++ src/main/java/com/github/pablowyourmind/sftptest/SftpTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.pablowyourmind.sftptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pablowyourmind/sftptest/SftpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pablowyourmind.sftptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
